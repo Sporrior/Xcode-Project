e003ebb196a8c8e32587c3c5d001aaf0cfffcb77
Sporrior <120579307+Sporrior@users.noreply.github.com>
2024-01-12 22:26:15 +0100

    Initial Commit

---
+++ Xcode Project/Xcode_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_ProjectApp.swift
+//  Xcode Project
+//
+//  Created by Damien Engelen on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
